d2a5e7ea4ef975e7b5641886dc248a3118757825
Qualeafclover <64626898+Qualeafclover@users.noreply.github.com>
2026-06-15 13:06:37 +0900

    simplified schema

--- database/schema.sql
+++ database/schema.sql
@@ -3,8 +3,7 @@ DROP TABLE IF EXISTS aisles;
 
 CREATE TABLE aisles (
     id SERIAL PRIMARY KEY,
-    aisle_label TEXT NOT NULL,
-    description TEXT
+    aisle_label TEXT NOT NULL
 );
 
 CREATE TABLE products (
@@ -13,17 +12,16 @@ CREATE TABLE products (
     description TEXT,                    
     price NUMERIC(10, 2) NOT NULL,       
     aisle_id INTEGER REFERENCES aisles(id) 
-)
-WITH inserted_aisles AS (
-    INSERT INTO aisles (aisle_label) VALUES 
-    ('A-1'), 
-    ('B-5'), 
-    ('家電エリア')
-    RETURNING id, aisle_label
-)
+);
+
+INSERT INTO aisles (aisle_label)
+VALUES
+    ('A-1'),
+    ('B-5'),
+    ('家電エリア');
+
 INSERT INTO products (name, description, price, aisle_id)
-SELECT 'ノートパソコン', '高性能で軽量なビジネス向けPC', 85000, id FROM inserted_aisles WHERE aisle_label = '家電エリア'
-UNION ALL
-SELECT 'ミネラルウォーター', '富士山の天然水 500ml', 100, id FROM inserted_aisles WHERE aisle_label = 'A-1'
-UNION ALL
-SELECT 'ぬいぐるみ', '肌触りの良いクマのぬいぐるみ', 1500, id FROM inserted_aisles WHERE aisle_label = 'B-5';
+VALUES
+    ('ノートパソコン', '高性能で軽量なビジネス向けPC', 85000, (SELECT id FROM aisles WHERE aisle_label = '家電エリア')),
+    ('ミネラルウォーター', '富士山の天然水 500ml', 100, (SELECT id FROM aisles WHERE aisle_label = 'A-1')),
+    ('ぬいぐるみ', '肌触りの良いクマのぬいぐるみ', 1500, (SELECT id FROM aisles WHERE aisle_label = 'B-5'));
