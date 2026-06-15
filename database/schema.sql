2a2c6d03544de109d8b713c0d4a7fcb634a6ec27
yuta-ai161210 <yuta171120@gmail.com>
2026-06-15 11:36:51 +0900

    a

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS aisles;
 CREATE TABLE aisles (
     id SERIAL PRIMARY KEY,
     aisle_label TEXT NOT NULL,
-    floor_level INTEGER DEFAULT 1,
     description TEXT
 );
 
@@ -28,11 +27,3 @@ UNION ALL
 SELECT 'ミネラルウォーター', '富士山の天然水 500ml', 100, id FROM inserted_aisles WHERE aisle_label = 'A-1'
 UNION ALL
 SELECT 'ぬいぐるみ', '肌触りの良いクマのぬいぐるみ', 1500, id FROM inserted_aisles WHERE aisle_label = 'B-5';
-
-SELECT 
-    p.name AS 商品名, 
-    p.description AS 詳細情報,
-    p.price AS 価格,
-    a.aisle_label AS 場所
-FROM products p
-JOIN aisles a ON p.aisle_id = a.id;
